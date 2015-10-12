12fac4a7a340c5027b575799fbaf1de770f52a87
Yamila Aramendia <yami_dreamer@hotmail.com>
2015-10-12 19:14:04 -0300

    Final Project finish

--- public/Controllers/NavController.js
+++ public/Controllers/NavController.js
@@ -1,19 +1,8 @@
 var app=angular.module('MenuController',[]);
 
 app.controller('NavController',['$scope', '$location' ,function($scope,$location){
-
+	
 	$scope.isActive=function (url) {
 		return url === $location.path();
 	};
-
-	$scope.view=function (url) {
-		if (url == '/')
-			$scope.view='Home';
-		else if (url == '/blockedUsers')
-				$scope.view='Blocked Users';
-			else if (url == '/trends')
-				$scope.view='Trends';
-			else
-				$scope.view="Twitter";
-	}
 }]);
